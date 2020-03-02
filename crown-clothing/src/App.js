92cc5922d3513cb0b4636d183c3343998f374264
Chris Honeycutt <clhoneycutt83@gmail.com>
2020-03-02 01:53:24 -0500

    Created and styled Homepage and menu components

---
+++ crown-clothing/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+
+import HomePage from './pages/homepage/homepage.component.jsx';
+
+function App() {
+  
+  return <div>
+    <HomePage/>
+  </div>
+    
+  
+}
+
+export default App;
