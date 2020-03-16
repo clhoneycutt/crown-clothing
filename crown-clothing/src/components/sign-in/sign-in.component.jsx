9a5f3404c642f6da37528ee0c4b5cc2e9bf272ca
Chris Honeycutt <clhoneycutt83@gmail.com>
2020-03-16 15:57:27 -0400

    Completed sign in with google button.

--- crown-clothing/src/components/sign-in/sign-in.component.jsx
+++ crown-clothing/src/components/sign-in/sign-in.component.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-import FormInput from '../form-input/form-input.component';
-import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.util';
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
+import { signInWithGoogle } from "../../firebase/firebase.utils";
 
 import "./sign-in.styles.scss";
 
@@ -51,8 +51,14 @@ class SignIn extends React.Component {
             label="password"
             required
           />
-          <CustomButton type="submit" value="Submit Form">Sign In</CustomButton>
-          <CustomButton onClick={ signInWithGoogle }>Sign In with Google</CustomButton>
+          <div className="buttons">
+            <CustomButton type="submit" value="Submit Form">
+              Sign In
+            </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign In with Google
+            </CustomButton>
+          </div>
         </form>
       </div>
     );
